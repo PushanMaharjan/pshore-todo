0a07bc49a75d1c4e973a5237deed867b226ee01d
PushanMaharjan <pushanm11@gmail.com>
2025-06-06 14:45:06 +0545

    add env examples

--- todo-front/src/services/todo.jsx
+++ todo-front/src/services/todo.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/todos";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getTodos = async (filter = "") => {
   const res = await axios.get(`${API_URL}${filter ? `?filter=${filter}` : ""}`);
