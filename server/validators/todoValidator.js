fce4b2d0236891a6cd3ee5ebf2c6e97612144a5f
PushanMaharjan <pushanm11@gmail.com>
2025-06-06 15:34:31 +0545

    chore: add minimum validation

--- server/validators/todoValidator.js
+++ server/validators/todoValidator.js
@@ -1,9 +1,9 @@
 import Joi from 'joi';
 
 export const todoValidation = Joi.object({
-  task: Joi.string().max(50).required(),
+  task: Joi.string().min(1).max(50).required(),
   completed: Joi.boolean().optional(),
-  short_desc: Joi.string().max(150).required(),
+  short_desc: Joi.string().min(1).max(150).required(),
   date_and_time: Joi.date().required()
 });
 
